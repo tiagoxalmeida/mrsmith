7bf2bd9789d6cb1d393c631abb8b3e8ef4e61ae3
Tiago Almeida <tiagoalmeida.pro@gmail.com>
2021-04-29 22:13:38 +0100

    Phone compatibility

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 </html>
     <head>
         <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/login.css">
         <title>Mr. Smith - Login</title>

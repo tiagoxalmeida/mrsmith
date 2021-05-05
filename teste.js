bbe07609c88f3a41c786353a9be213faf1d8124e
Dmrv2000 <Dmrv2000@Outlook.pt>
2021-05-05 17:29:27 +0100

    cenas Please enter the commit message for your changes. Lines starting

--- teste.js
+++ teste.js
@@ -1,18 +1,4 @@
-var PubKey;
-var chave;
 
 
-document.getElementById("gerar").onclick = function(){
-    chave = cryptico.generateRSAKey("password",1024);
-    PubKey = cryptico.publicKeyString(chave);
-    document.getElementById("teste").innerHTML = PubKey;
-};
 
-document.getElementById("encrypt").onclick = function(){
-    var plaintext = document.getElementById("input1").value;
-    var ciphertext = cryptico.encrypt(plaintext,PubKey);
-    var plaintext2 = cryptico.decrypt(ciphertext.cipher,chave);
-    document.getElementById("teste").innerHTML = ciphertext.cipher;
-    document.href = "?cipher=" + ciphertext + "&sk=" + chave; 
-};
 

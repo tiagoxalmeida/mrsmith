4ee1d2fcad0850b73cbe365d331e0b0ca269376d
Chaseroffice <64603836+Chaseroffice@users.noreply.github.com>
2021-05-20 16:49:31 +0100

    Add files via upload

---
+++ help/header.php
@@ -0,0 +1,15 @@
+<?php
+
+
+?>
+
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<div class="navbar">
+<a href="/">HomePage </a>
+<a href="index.php">Início</a>
+<a href="perguntasfrequentes.php">Perguntas Frequentes</a>
+	
+</div>
